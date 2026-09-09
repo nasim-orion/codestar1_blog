31935933ead058840ef9dabe5f1822b3f4d1f4dd
nasim-orion <nasimch2709@gmail.com>
2026-09-09 03:31:39 +0100

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dmp5$w3x4wgwvb_scsjipuf58le$k=)@0)(c8ysb)2u$20$i2d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DJANGO_DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = [,'.herokuapp.com']
 
